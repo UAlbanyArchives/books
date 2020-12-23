7764de0b1c569d3b365c1c6b26685b351e20fee8
Katherine Mules <kmules@albany.edu>
2020-12-23 14:19:07 -0500

    set up under subpath

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+scope 'mathes' do
+
   concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
   mount Blacklight::Engine => '/'
   root to: "catalog#index"
@@ -24,4 +26,5 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
 end
