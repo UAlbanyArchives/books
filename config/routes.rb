830de4a907281215f4e9551f1eeab2fe2f8dd66c
Greg Wiedeman <gregory.wiedeman1@gmail.com>
2023-03-28 15:40:11 -0400

    config files updated and tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+#Rails.application.routes.draw do
+#  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#end
+
 Rails.application.routes.draw do
 
 scope 'books' do
@@ -26,5 +30,5 @@ scope 'books' do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-end
+  end
+  end
