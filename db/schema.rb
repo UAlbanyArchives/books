30ca2e979d30d3fff709f2e16c6d50df6fcd1652
kmules1 <50674690+kmules1@users.noreply.github.com>
2025-06-18 11:25:54 -0400

    initial config for dockerizing the app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_28_160255) do
+ActiveRecord::Schema.define(version: 2020_12_23_182952) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
